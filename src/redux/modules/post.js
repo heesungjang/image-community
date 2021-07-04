8210291e5a152932a631506f2d698dc44ea61307
Heesung jang <heesungj7@gmail.com>
2021-07-05 01:03:52 +0900

    20210705 - 상세 페이지 + 무한 스크롤 기능 추가

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -9,16 +9,22 @@ import { actionCreators as imageActions } from "./image";
 const SET_POST = "SET_POST";
 const ADD_POST = "ADD_POST";
 const EDIT_POST = "EDIT_POST";
+const LOADING = "LOADING";
 
-const setPost = createAction(SET_POST, (post_list) => ({ post_list }));
+const setPost = createAction(SET_POST, (post_list, paging) => ({
+    post_list,
+    paging,
+}));
 const addPost = createAction(ADD_POST, (post) => ({ post }));
 const editPost = createAction(EDIT_POST, (post_id, post) => ({
     post_id,
     post,
 }));
-
+const loading = createAction(LOADING, (is_loading) => ({ is_loading }));
 const initialState = {
     list: [],
+    paging: { start: null, next: null, size: 3 },
+    is_loaded: false,
 };
 
 const initialPost = {
@@ -161,40 +167,62 @@ const addPostFB = (contents = "") => {
     };
 };
 
-const getPostFB = () => {
+const getPostFB = (start = null, size = 3) => {
     return function (dispatch, getState, { history }) {
+        let _paging = getState().post.paging;
+
+        if (_paging.start && !_paging.next) {
+            return;
+        }
+
+        dispatch(loading(true));
         const postDB = firestore.collection("post");
 
-        let query = postDB.orderBy("insert_dt", "desc").limit(2);
+        let query = postDB.orderBy("insert_dt", "desc");
 
-        query.get().then((docs) => {
-            let post_list = [];
-            docs.forEach((doc) => {
-                let _post = doc.data();
-                // ['commenct_cnt', 'contents', ..]
-                let post = Object.keys(_post).reduce(
-                    (acc, cur) => {
-                        if (cur.indexOf("user_") !== -1) {
-                            return {
-                                ...acc,
-                                user_info: {
-                                    ...acc.user_info,
-                                    [cur]: _post[cur],
-                                },
-                            };
-                        }
-                        return { ...acc, [cur]: _post[cur] };
-                    },
-                    { id: doc.id, user_info: {} }
-                );
+        if (start) {
+            query = query.startAt(start);
+        }
+        query
+            .limit(size + 1)
+            .get()
+            .then((docs) => {
+                let post_list = [];
 
-                post_list.push(post);
+                let paging = {
+                    start: docs.docs[0],
+                    next:
+                        docs.docs.length === size + 1
+                            ? docs.docs[docs.docs.length - 1]
+                            : null,
+                    size: size,
+                };
+
+                docs.forEach((doc) => {
+                    let _post = doc.data();
+                    // ['commenct_cnt', 'contents', ..]
+                    let post = Object.keys(_post).reduce(
+                        (acc, cur) => {
+                            if (cur.indexOf("user_") !== -1) {
+                                return {
+                                    ...acc,
+                                    user_info: {
+                                        ...acc.user_info,
+                                        [cur]: _post[cur],
+                                    },
+                                };
+                            }
+                            return { ...acc, [cur]: _post[cur] };
+                        },
+                        { id: doc.id, user_info: {} }
+                    );
+
+                    post_list.push(post);
+                });
+
+                post_list.pop();
+                dispatch(setPost(post_list, paging));
             });
-
-            console.log(post_list);
-
-            dispatch(setPost(post_list));
-        });
 
         return;
         postDB.get().then((docs) => {
@@ -232,7 +260,9 @@ export default handleActions(
     {
         [SET_POST]: (state, action) =>
             produce(state, (draft) => {
-                draft.list = action.payload.post_list;
+                draft.list.push(...action.payload.post_list);
+                draft.paging = action.payload.paging;
+                draft.is_loading = false;
             }),
 
         [ADD_POST]: (state, action) =>
@@ -249,6 +279,10 @@ export default handleActions(
                     ...draft.list[idx],
                     ...action.payload.post,
                 };
+            }),
+        [LOADING]: (state, action) =>
+            produce(state, (draft) => {
+                draft.is_loading = action.payload.is_loading;
             }),
     },
     initialState
